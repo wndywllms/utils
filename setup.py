de599ab6fad3c69d643780722714a8a41f557f08
Wendy L. Williams <wndywllms@gmail.com>
2017-02-16 16:22:48 +0000

    add simple setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(author='Wendy L. Williams',
+      author_email='wndywllms@gmail.com',
+      url='https://github.com/wllwen007/utils',
+      name='utils',
+      version='1.0',
+      py_modules=['utils'],
+      )
